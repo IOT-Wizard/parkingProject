2e4362343cca2e9fb6397807841891605b19e94e
HAITAME <haitame.laframe@etu.uae.ac.ma>
2023-11-14 17:06:11 +0100

    navbar

---
+++ client/src/pages/home/home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./home.scss";
+
+c
+onst Home = () => {
+    return (
+      <div className='home'>
+       Home page
+      </div>
+    )
+  }
+  
+  export default Home
