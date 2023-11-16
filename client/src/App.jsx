b342bf58f75d6193f7389dbe0a91eb906fbbd6a2
maroua <marouaamal2002@gmail.com>
2023-11-16 14:29:47 +0100

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,18 +2,18 @@ import "./app.scss";
 
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 //import React from "react";
-/*import Navbar from "./components/navbar/Navbar";*/
-import Home from "./pages/home/Home";
+import Navbar from "./components/navbar/Navbar";
 /*import Footer from "./components/footer/footer";*/
 import SignUp from "./pages/signUp/signUp";
 import SignIn from "./pages/signIn/signIn";
+import Home from "./pages/home/home";
 
 
 function App() {
   const Layout = () => {
     return (
       <div className="app">
-        
+        <Navbar/>
         <Outlet />
        
       </div>
--- service/main.py
+++ service/main.py
@@ -1,29 +1,30 @@
-from flask import Flask 
+from flask import Flask
 import mysql.connector
-from tables import create_user_table , create_cars_table , create_parking_history_table , create_subscription_table
+from tables import create_user_table, create_cars_table, create_parking_history_table, create_subscription_table
 
+# Database connection
 mydb = mysql.connector.connect(
     host="127.0.0.1",
     user="root",
-    password="0802",
+    password="",  # Remove this line if there's no password
     database="parking"
 )
 
+# Flask application (optional for database setup)
 app = Flask(__name__)
 
+# Get a cursor object from the connection
 cursor = mydb.cursor()
 
+# Execute table creation statements
 cursor.execute(create_user_table)
 cursor.execute(create_cars_table)
 cursor.execute(create_parking_history_table)
 cursor.execute(create_subscription_table)
 
+# Commit the changes to the database
 mydb.commit()
 
-# Members API Route
-@app.route("/")
-def members():
-    return {"members": ["Member1", "Member2", "Member3"]}
-
-if __name__ == "__main__": 
-    app.run(debug=True, host="0.0.0.0", port=5000)
+# Close the cursor and database connection
+#cursor.close()
+#mydb.close()
