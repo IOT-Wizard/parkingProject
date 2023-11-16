ebef5c3b0e73f07e92fea32a181f58614418350d
SoukainaElidrissi <soukainaedrissi2002@gmail.com>
2023-11-16 17:00:44 +0100

    login signup

--- service/main.py
+++ service/main.py
@@ -1,16 +1,18 @@
-from flask import Flask 
+from flask import Flask, jsonify, request 
 import mysql.connector
 from tables import create_user_table , create_cars_table , create_parking_history_table , create_subscription_table
+from flask_cors import CORS
+
 
 mydb = mysql.connector.connect(
     host="127.0.0.1",
     user="root",
-    password="0802",
+    password="",
     database="parking"
 )
 
 app = Flask(__name__)
-
+CORS(app)
 cursor = mydb.cursor()
 
 cursor.execute(create_user_table)
@@ -24,6 +26,55 @@ mydb.commit()
 @app.route("/memebrers")
 def members():
     return {"members": ["Member1", "Member2", "Member3"]}
+
+
+
+
+
+@app.route("/signin", methods=["POST"])
+def login():
+    data = request.get_json()
+    username = data.get("username")
+    password = data.get("password")
+
+    # Perform authentication logic (compare with database, etc.)
+    # For simplicity, let's assume you have a 'users' table in your database
+
+    query = "SELECT * FROM users WHERE username = %s AND password = %s"
+    cursor.execute(query, (username, password))
+    user = cursor.fetchone()
+
+    if user:
+        # Authentication successful
+        return jsonify({"message": "Login successful", "user_id": user[0]})
+    else:
+        # Authentication failed
+        return jsonify({"message": "Invalid credentials"}), 401
+
+
+
+@app.route("/signup", methods=["POST"])
+def signup():
+    data = request.get_json()
+    full_name = data.get("full_name")
+    username = data.get("username")
+    password = data.get("password")
+
+    # Check if the username is already taken
+    check_query = "SELECT * FROM users WHERE username = %s"
+    cursor.execute(check_query, (username,))
+    existing_user = cursor.fetchone()
+
+    if existing_user:
+        return jsonify({"message": "Username already taken"}), 400  # 400 Bad Request
+
+    # Insert the new user into the database
+    insert_query = "INSERT INTO users (full_name, username, password) VALUES (%s, %s, %s)"
+    cursor.execute(insert_query, (full_name, username, password))
+    mydb.commit()
+
+    return jsonify({"message": "Signup successful"}), 201  # 201 Created
+
 
 if __name__ == "__main__": 
     app.run(debug=True, host="0.0.0.0", port=5000)
