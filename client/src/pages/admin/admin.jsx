be57ccb605402c8bb69dfdbb5a4bab23400176e0
maroua <marouaamal2002@gmail.com>
2023-11-22 21:48:54 +0100

    c

--- client/src/pages/admin/admin.jsx
+++ client/src/pages/admin/admin.jsx
@@ -1,12 +1,23 @@
+import React from 'react';
+import { Button, Card } from 'antd';
+import { Link } from 'react-router-dom';
+
+const Admin = () => {
+  const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+
+  return (
+    <div style={{ padding: '20px' }}>
+      <Card title={`Bienvenue, ${currentUser.username}`}>
+        <Button type="primary" style={{ marginRight: '10px' }}>
+          <Link to="/">Rapport</Link>
+        </Button>
+        <Button type="primary">
+          <Link to="/addbadge">AddBadge</Link>
+        </Button>
+      </Card>
+    </div>
+  );
+};
 
 
-const Admin = () =>{
-    return(
-        <div>
-            
-            
-        </div>
-    )
-
-}
 export default Admin;
