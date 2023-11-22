0dfa73d9aba2570f0ad58b9465e3924953b040c0
maroua <marouaamal2002@gmail.com>
2023-11-22 16:57:19 +0100

    c

--- serviceCam/mainC.py
+++ serviceCam/mainC.py
@@ -1,7 +1,7 @@
 from flask import Flask
-#from flask_sqlalchemy import SQLAlchemy
 import pytesseract
 import cv2
+import threading
 from datetime import datetime
 import mysql.connector
 
@@ -15,13 +15,8 @@ db_config = {
     'database': 'parking'
 }
 
-# Establish a MySQL connection
-conn = mysql.connector.connect(**db_config)
-cursor = conn.cursor()
-
-# Your text detection route
-@app.route('/detect_text')
-def detect_text():
+# Function to handle text detection
+def perform_text_detection():
     # Open the webcam
     cap = cv2.VideoCapture(0)
 
@@ -40,22 +35,25 @@ def detect_text():
             # Insert the detected text into the 'cars' table
             query = "INSERT INTO cars (detected_text, car_owner_id) VALUES (%s, %s)"
             values = (imgchar, 1)  # Assuming car_owner_id is 1 for this example
-            cursor.execute(query, values)
-            conn.commit()
+            try:
+                cursor.execute(query, values)
+                conn.commit()
+            except mysql.connector.Error as err:
+                print(f"Error: {err}")
 
-            
             # Use the binarized image for box detection
             imgboxes = pytesseract.image_to_boxes(thresh)
-            
+
             for boxes in imgboxes.splitlines():
                 boxes = boxes.split(' ')
                 x, y, w, h = int(boxes[1]), int(boxes[2]), int(boxes[3]), int(boxes[4])
                 cv2.rectangle(frame, (x, imgH - y), (w, imgH - h), (0, 0, 255), 3)
 
-            cv2.putText(frame, imgchar, (x1 + int(w1 / 50), y1 + int(h1 / 50)), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
+            cv2.putText(frame, imgchar, (x1 + int(w1 / 50), y1 + int(h1 / 50)), cv2.FONT_HERSHEY_SIMPLEX, 0.7,
+                        (255, 0, 0), 2)
 
             cv2.imshow('Text Detection Tutorial', frame)
-            
+
             # Print detected text to the console
             print("Detected Text:")
             print(imgchar)
@@ -66,12 +64,24 @@ def detect_text():
             if cv2.waitKey(2) & 0xFF == ord('q'):
                 break
 
-    # Close the webcam, release the database connection, and shut down the app
+    # Close the webcam, release the database connection
     cap.release()
     cursor.close()
     conn.close()
 
-    return "Text detection complete!"
+# Your text detection route
+@app.route('/detection')
+def detect_text():
+    # Start the text detection in a separate thread
+    detection_thread = threading.Thread(target=perform_text_detection)
+    detection_thread.start()
 
-if __name__ == "__main__": 
+    return "Text detection started!"
+
+if __name__ == "__main__":
+    # Establish a MySQL connection
+    conn = mysql.connector.connect(**db_config)
+    cursor = conn.cursor()
+
+    # Run the Flask app in debug mode on 0.0.0.0:5000
     app.run(debug=True, host="0.0.0.0", port=5000)
